369a87c1fa5f7c4ea41cee9af7314c50cccaf505
Jose Sanchez-Capo <josesanchezcapo@me.com>
2021-01-09 20:18:20 -0600

    Adding view all employee logic

--- app.js
+++ app.js
@@ -1,54 +1,88 @@
 // adding depentencies
-const mysql = require("mysql");
-const inquirer = require("inquirer");
-const consoleTable = require("console.table");
-const { exclude } = require("inquirer/lib/objects/separator");
-const Choices = require("inquirer/lib/objects/choices");
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+//const { exclude } = require('inquirer/lib/objects/separator');
+//const Choices = require('inquirer/lib/objects/choices');
 
 // database connection 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'Sql01!!??',
-    database: 'employee_tracker'
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'Sql01!!??',
+  database: 'employee_tracker'
 });
 
 // open connection
-connection.connect(function(err) {
-    if (err) {
-      return console.error('error: ' + err.message);
+connection.connect(function (err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+});
+
+// main menu
+
+console.log('\n EMPLOYEE TRACKER \n');
+
+const mainMenu = function () {
+  inquirer.prompt([
+    {
+
+      type: 'list',
+      name: 'menuChoises',
+      message: 'Employee Tracker : Main Menu',
+      choices: [
+        'View all employees',
+        'View all department',
+        'View all roles',
+        'Add Department',
+        'Add Role',
+        'Add Employee',
+        'Update employee roles',
+      ]
     }
-  
-    // Testing - console.log('Connected to the MySQL server.');
-    
+  ]).then((responses) => {
+
+    switch (responses.menuChoises) {
+      case 'View all employees':
+        viewAllEmployees();
+        break;
+      case 'View all employees':
+        viewAllDepartments();
+        break;
+    }
   });
 
-  function mainMenu(){
+  function viewAllEmployees() {
 
-    inquirer.prompt({
-      name: 'action',
-      type: 'list',
-      message: 'Employee Tracker : Main Menu',
-      Choices: [
-        'all employees',
-        'View all employees by role',
-        'View all employees by department',
-        'View all employees by manager',
-        "Add employee",
-        "Add role",
-        "Add department",
-        "Update employee role",
-        "Update employee manager",
-        "Delete employee",
-        "Delete role",
-        "Delete department",
-        "View department budgets"
-      ]
-      
-    }) .then((responses) => {
+    const query = "SELECT " +
+      "employee.id AS 'Employee Id', " +
+      "employee.first_name  AS 'First Name', " +
+      "employee.last_name AS 'Last Name', " +
+      "IFNULL(role.title, ' ') AS 'Title', " +
+      "IFNULL(CONCAT(emp.first_name, ' ' ,emp.last_name),' ') AS 'Manager' " +
+      "FROM employee " +
+      "LEFT JOIN role on employee.role_id =  role.id " +
+      "LEFT JOIN employee as emp on employee.id = emp.manager_id;"
+
+    connection.query(query, function (err, res) {
+      if (err) return err;
+      console.log("\n");
+      console.table(res);
+      mainMenu();
+
+    });
+
+    function viewAllDepartments() {
+
+      const query = "";
 
 
-    })
-  }
+    };
 
+  };
+
+};
+
+mainMenu();
